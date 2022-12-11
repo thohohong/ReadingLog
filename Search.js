5d00dba1a9c283f7b96887786a52fbe8fb73bb8f
thohohong <so0426yeon@gmail.com>
2022-12-12 02:26:17 +0900

    Get Request from Aladin Open API

---
+++ Search.js
@@ -0,0 +1,55 @@
+import {React, useEffect, useState} from 'react';
+import {View, Text, Image, TextInput, Button} from 'react-native';
+import axios from 'axios';
+import config from './Apikey';
+
+const setURL = (keyword) => {
+  var URL = config.aladinBasicURL + "?ttbkey=" + config.aladinKey + "&Query=" + keyword + "&QueryType=keyword&MaxResults=10&start=1&SearchTarget=Book&output=js&Version=20070901";
+  console.log(URL);
+  return URL;
+}
+
+const Search = () => {
+  const [keyword, setKeyword] = useState("");
+  const [bookData, setBookData] = useState("");
+
+  const processBookData = (data) => {
+    data = data.replaceAll('\\', '');
+    data = data.substr(0, data.length-1);
+
+    setBookData(JSON.parse(data).item);    
+  }
+
+  return (
+    <View style={{flex: 1}}>
+      <TextInput
+        onChangeText={setKeyword}
+        placeholder="검색어를 입력해주세요."
+      />
+      <Button
+        title="검색"
+        onPress={()=>{
+          axios.get(setURL(keyword))
+            .then((response) => {
+              processBookData(response.data);
+            })
+            .catch((error) => {
+              console.log(error);
+            })
+        }}
+      />
+
+      <View>
+        {Object.values(bookData).map((book, index)=>(
+          <View key={index}>
+            <Text>
+              {book.title}
+            </Text>
+          </View>
+        ))}
+      </View>
+    </View>
+  )
+}
+
+export {Search};
